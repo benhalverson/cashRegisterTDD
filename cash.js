0613d8b599ff099fee4e18ee0ccfce19b00822fd
Ben Halverson <benhalverson33@gmail.com>
2015-05-02 04:20:46 -0700

    tests pass

--- cash.js
+++ cash.js
@@ -1,7 +1,27 @@
-var Cash = {};
-Cash.getChange = function () {
-  'use strict';
-  return true;
+var Cash = {};     // C Object simplifies exporting the module
+Cash.coins = [5000, 2000, 1000, 500, 200, 100, 50, 20, 10, 5, 2, 1];
+
+
+Cash.getChange = function (totalPayable, cashPaid) {
+    'use strict';
+
+    var change = [], length = Cash.coins.length,
+    remaining = cashPaid - totalPayable; // we reduce this below
+    for (var i = 0; i < length; i++) { // loop through array of notes & coins:
+        var coin = Cash.coins[i];
+
+        if(remaining/coin >= 1) { // check coin fits into the remaining amount
+            var times = Math.floor(remaining/coin); // no partial coins
+            // console.log('Coin: '+coin+' fits in '+remaining +' x ' +times);
+
+            for(var j = 0; j < times; j++) { // add coin to change array x times
+                change.push(coin);
+                remaining = remaining - coin; // reduce remaining amount by coin
+            }
+        }
+    }
+    // console.log(change); // print change array to console for feedback
+    return change;
 };
 
-module.exports = Cash;
+module.exports = Cash;         // export the module with a single method
--- test/test.js
+++ test/test.js
@@ -8,8 +8,24 @@ describe('Cash Register', function(){
       assert.equal(typeof Cash, 'object');
       assert.equal(typeof Cash.getChange, 'function');
     });
-    it('getChange(100,300) should equal [25, 10, 5]', function(){
-      assert.deepEqual(Cash.getChange(100,300), [25, 10, 5]);
-    })
+    it('getChange(210,300) should equal [50,20,20]', function(){
+        assert.deepEqual(Cash.getChange(210,300), [50, 20, 20]);
+    });
+
+    it('getChange(486,1000) should equal [500, 10, 2, 2]', function(){
+        assert.deepEqual(Cash.getChange(486,1000), [500, 10, 2, 2]);
+    });
+
+    it('getChange(1487,10000) should equal [5000, 2000, 1000, 500, 10, 2, 1 ]', function(){
+    assert.deepEqual(Cash.getChange(1487,10000), [5000, 2000, 1000, 500, 10, 2, 1 ]);
+    });
+
+    // try to force an error:
+    it('getChange("random","str") to return [] (empty array)', function(){
+    // assert.throw(C.getChange("random","str"), Error, "totalPayable and cashPaid MUST both be numbers");
+    assert.deepEqual(Cash.getChange("random","str"), []);
+
+});
+
   });
 });
